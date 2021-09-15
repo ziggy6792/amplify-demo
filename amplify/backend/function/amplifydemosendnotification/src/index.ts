36381d123d86cae00a15ec1e43c3c64c3ff198c5
simon-verhoeven <simonaverhoeven@gmail.com>
2021-09-15 16:41:32 +0800

    add typescript, lerna, eslint

--- amplify/backend/function/amplifydemosendnotification/src/index.ts
+++ amplify/backend/function/amplifydemosendnotification/src/index.ts
@@ -4,16 +4,17 @@
 	MY_ENV_PARAM
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
+exports.handler = async (event: any) => {
+  console.log('Hello World bla bla bla');
+  // TODO implement
+  const response = {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
     //  headers: {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    //  },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
 };
