8a4bcf579f245625f0409f27b159ad352c29b295
simon-verhoeven <simonaverhoeven@gmail.com>
2021-09-15 19:12:52 +0800

    send telegram notifications

--- amplify/backend/function/amplifydemosendnotification/src/index.ts
+++ amplify/backend/function/amplifydemosendnotification/src/index.ts
@@ -1,16 +1,38 @@
+/* eslint-disable import/prefer-default-export */
 /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
 	MY_ENV_PARAM
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event: any) => {
+import TelegramBot from 'node-telegram-bot-api';
+
+const TELEGRAM_CONFIG = {
+  APT_KEY: '1985947273:AAF5rzAk6Pt0jzwkjoGWjzOQ2NbTRRKqcBE',
+  CHAT_ID: '-520673898',
+};
+
+const bot = new TelegramBot(TELEGRAM_CONFIG.APT_KEY, { polling: false });
+
+const sendNotification = async (message: string, chatId = TELEGRAM_CONFIG.CHAT_ID, isMarkDown = true): Promise<TelegramBot.Message> => {
+  let result: TelegramBot.Message;
+  try {
+    result = await bot.sendMessage(chatId, message, { parse_mode: isMarkDown ? 'Markdown' : undefined, disable_web_page_preview: true });
+    console.log({ result });
+  } catch (err) {
+    console.log({ err });
+  }
+  return result;
+};
+
+export const handler = async (event: any) => {
   const isLocal = process.env.AWS_EXECUTION_ENV === 'AWS_Lambda_amplify-mock';
   const isProd = process.env.ENV === 'prod';
 
   console.log('isLocal', isLocal);
   console.log('isProd', isProd);
   console.log('Hello World bla bla bla');
+  await sendNotification('hello from lambda');
 
   // TODO implement
   const response = {
